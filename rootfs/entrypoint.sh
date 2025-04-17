36fdac97be1992bb05d1a5ed2dbbb9628b48b1f0
Grisha <grisha765@tuta.io>
2025-04-17 20:50:49 +0300

    [u] link to downloads dir

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -5,10 +5,12 @@ set -eu
 : "${USER:?env USER is not set}"
 
 mkdir -p /config/downloads
+ln -s /config/downloads /home/$USER/downloads
 mkdir -p /config/log/firefox
 mkdir -p /config/profile
 
 chown -R $USER:$USER /config
+ln -s /home/$USER/downloads
 
 if [ "${FF_OPEN_URL:-}" ]; then
   echo $FF_OPEN_URL > /tmp/open-url
