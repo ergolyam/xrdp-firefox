03ccf434ac1ec4ec009596257566d74c6b3408af
ergolyam <grisha765@tuta.io>
2026-07-13 23:00:09 +0300

    [f] fix preparation script

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,19 +3,31 @@
 set -eu
 
 : "${USER:?env USER is not set}"
+: "${USER_UID:?env USER_UID is not set}"
+: "${USER_GID:?env USER_GID is not set}"
 
 DATA_DIR="/data"
-USER_LINK="/home/$USER/downloads"
+USER_LINK="/home/${USER}/downloads"
 
-mkdir -p ${DATA_DIR}/downloads
-mkdir -p ${DATA_DIR}/log/firefox
-mkdir -p ${DATA_DIR}/profile
+ensure_owned_dir() {
+    dir="$1"
+
+    mkdir -p "${dir}"
+
+    if [ "$(stat -c '%u:%g' "${dir}")" != "${USER_UID}:${USER_GID}" ]; then
+        echo "Changing access rights for the ${dir} directory"
+        chown -R "${USER_UID}:${USER_GID}" "${dir}"
+    fi
+}
+
+ensure_owned_dir "${DATA_DIR}"
+ensure_owned_dir "${DATA_DIR}/downloads"
+ensure_owned_dir "${DATA_DIR}/log/firefox"
+ensure_owned_dir "${DATA_DIR}/profile"
 
 if [ ! -e "${USER_LINK}" ] && [ ! -L "${USER_LINK}" ]; then
     ln -s "${DATA_DIR}/downloads" "${USER_LINK}"
 fi
-
-chown -R $USER:$USER ${DATA_DIR}
 
 if [ "${FF_OPEN_URL:-}" ]; then
   echo $FF_OPEN_URL > /tmp/open-url
